99cf716d44711735126acbc026e8eb953fa04bda
Husnain Ahmad <loedex@yahoo.com>
2026-08-04 23:28:18 +0500

    Added FeatureCard component with hover effects.

---
+++ 05-tailwind-css/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FeatureCard from './components/FeatureCard';
+
+const App = () => {
+  return (
+    <div>
+     
+      <FeatureCard />
+    </div>
+  );
+}
+
+export default App
---
+++ 05-tailwind-css/src/components/FeatureCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FeatureCard = () => {
+  return (
+    <div className='group border rounded-xl p-6 hover:shadow-lg transition cursor-pointer'>
+        <h1 className='group-hover:text-red-600 transition'>Heading</h1>
+        <p className='group-hover:text-green-600 group-hover:bg-yellow-500 transition'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus numquam rem ipsum. Quis enim iusto, sint exercitationem modi possimus incidunt fuga, doloribus, quae vero nisi provident? Sint impedit incidunt sapiente?</p>
+    </div>
+  )
+}
+
+export default FeatureCard
