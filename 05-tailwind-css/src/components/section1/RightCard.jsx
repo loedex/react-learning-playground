2fb39e435d625cab0cddf2d408f0eb2623a51606
Husnain Ahmad <loedex@yahoo.com>
2026-08-08 21:02:28 +0500

    Fix layout issues in RightCard component by adding overflow-hidden and removing unnecessary rounded class

--- 05-tailwind-css/src/components/section1/RightCard.jsx
+++ 05-tailwind-css/src/components/section1/RightCard.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const RightCard = ({ imgSrc, name = "Satisfied" }) => {
   return (
     <div
-      className={`h-full relative    w-3/8 rounded-4xl px-7 flex flex-col justify-between`}
+      className={`h-full relative  overflow-hidden   w-3/8 rounded-4xl px-7 flex flex-col justify-between`}
     >
       <img
         src={imgSrc}
-        className="w-full h-full absolute inset-0 -z-10 object-cover opacity-60 rounded-4xl"
+        className="w-full h-full absolute inset-0 -z-10 object-cover opacity-60"
         alt=""
       />
       <div className="mt-10 bg-amber-400 w-1 h-1 p-5 flex justify-center items-center rounded-full">
