eb38283f8d4f5a2bd1fa9d0c7e2bdb65ff2f1f73
Husnain Ahmad <loedex@yahoo.com>
2026-09-02 12:58:24 +0500

    add increment button and useState for count in App component

--- 11-useEfffect-1/src/App.jsx
+++ 11-useEfffect-1/src/App.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0);
   console.log("Component Rendered");
   useEffect(()=>{
     console.log("Effect ran after rendering");
-    
+    const intervalId = setInterval(() => {
+      console.log("interval line");
+      
+    }, 1000);
+
+    return ()=> clearInterval(intervalId);
   }, []);
   
   return (
     <div>
       <h1>Check the console to see the useEffect exeution order</h1>
+      <button onClick={()=> setCount(count+1)}>Increment</button>
     </div>
   )
 }
