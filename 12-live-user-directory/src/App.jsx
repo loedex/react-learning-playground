16a8b0672fa97aa41324a8f05872221d552d6f5b
Husnain Ahmad <loedex@yahoo.com>
2026-09-04 11:39:36 +0500

    add Live User Directory component that fetches dat from an API, with auto-refresh functionality

---
+++ 12-live-user-directory/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LiveUserDirectory from './components/LiveUserDirectory'
+
+const App = () => {
+  return (
+    <div>
+      <LiveUserDirectory />
+    </div>
+  )
+}
+
+export default App
---
+++ 12-live-user-directory/src/components/LiveUserDirectory.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect } from 'react'
+import { useState , } from 'react'
+
+const LiveUserDirectory = () => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [refreshCount, setRefreshCount] = useState(0);
+
+    //Effect-1
+    //Fetch data whenever the refreshCount changes
+    useEffect(()=> {
+        const fetchData = async ()=> {
+            try{
+                setIsLoading(true);
+                const response = await fetch('https://jsonplaceholder.typicode.com/users');
+                if(!response.ok) throw new Error("Failed to obtain user's data");
+                const data = await response.json();
+                setData(data);
+                
+            }catch(err){
+                setError(err.message);
+            }finally{
+                setIsLoading(false);
+            }
+        }
+        fetchData();
+    }, [refreshCount]);
+    
+
+    //Second useEffect to update refreshCount every 10 seconds
+    useEffect(()=>{
+        const timerId = setInterval(()=>{
+            setRefreshCount(prev => prev+1);
+        },10000);
+
+        //Stop timer on unmount
+        return ()=> clearInterval(timerId);
+    },[]); //Setup the timer once on mount only
+
+
+    //Effect-3 
+    //keep the browser tab's title in sync with current state
+    useEffect(()=>{
+        if(isLoading){
+            document.title = 'loading users ...';
+        }else if(error){
+            document.title = 'Error loading users';
+        }else{
+            document.title=`${data.length} users loaded`;
+        }
+    }, [error, data, isLoading]);
+    if(isLoading && data.length===0){
+        return <p style={{background: 'geen', color: 'white', fontWeight:'bold'}}>loading users data .... please wait, it should take a minute</p>
+    }
+    if(error){
+        return <p style={{background: 'red', color: 'white', fontWeight: 'bold'}}>Error occurred : {error}</p>
+    }
+
+console.log(data);
+
+  return (
+    <div style={{width: '100%', minHeight: '100vh', height: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column', }}>
+        <h1>Live User Directory </h1>
+        <h5>Auto refreshes after 10 seconds</h5>
+     <div style={{display: 'flex', gap: '20px', flexWrap: 'wrap', width: '100%'}}>
+           {
+            data.map((data)=> (
+               <div >
+                 <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '40px', backgroundColor: 'whitesmoke', padding: '40px',  }}>
+                    <h3>Name : {data.name}</h3>
+                    <h4>Username : {data.username}</h4>
+                    <h5>City : {data.address.city}</h5>
+                    <h6>Company : {data.company.name}</h6>
+                    <h6>Email : {data.email}</h6>
+                    <h6>Phone Number : {data.phone}</h6>
+                    <h6>Visit my website : {data.website}</h6>
+                </div>
+               </div>
+            ))
+        }
+     </div>
+    </div>
+  )
+}
+
+export default LiveUserDirectory
