d133967dffee5f41a72aa57bf91a7b7e6456de77
Husnain Ahmad <loedex@yahoo.com>
2026-09-07 22:30:28 +0500

    Add initial project setup with weather fetching functionality,

---
+++ 13-weather-watchlist-project/src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState } from 'react'
+import { CITY_COORDINATES } from './components/CityData'
+import CityCard from './components/CityCard';
+
+const App = () => {
+  const [watchlist, setWatchList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [formError, setFormError] = useState("");
+
+
+  function handleAddCity(e) {
+    e.preventDefault();
+
+    const key = searchText.trim().toLowerCase().replace(/\s/g, '');
+
+    if(!key){
+      setFormError('please type a city name');
+      return;
+    }
+
+    const match = CITY_COORDINATES[key];
+    if(!match){
+      setFormError(`${searchText}  is not in our demo list.` );
+      return;
+    }
+    setWatchList((prev)=> [...prev, match]);
+    setSearchText('');
+    setFormError('');
+  }
+  return (
+    <div>
+      <h1>Weather WatchList</h1>
+      <p>Auto refreshes every city's weather every 30 seconds</p>
+
+      <form onSubmit={handleAddCity}>
+        <input value={searchText} onChange={(e)=> setSearchText(e.target.value)} type="text" placeholder='Search city name ...' />
+        <button type='submit'>Add to watchlist</button>
+      </form>
+
+      {
+        formError && <p style={{color: 'red'}}>{formError}</p>
+      }
+
+
+      {
+        watchlist.length === 0 ? (
+          <p>No cities added yet - search above to get started.</p>
+        ) : (
+          <div>
+            {watchlist.map((city) => (
+              <CityCard key={city.name} city={city}/>
+            ))}
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ 13-weather-watchlist-project/src/components/CityData.jsx
@@ -0,0 +1,9 @@
+export const CITY_COORDINATES = {
+  lahore: { name: "lahore", lat: 31.55, lon: 74.35 },
+  karachi: { name: "karachi", lat: 24.68, lon: 67.01 },
+  islamabad: { name: "islamabad", lat: 33.72, lon: 73.06 },
+  london: { name: "london", lat: 51.51, lan: -0.13 },
+  newyork: { name: "New York", lat: 40.71, lon: -74.01 },
+  tokyo: { name: "Tokyo", lat: 35.68, lon: 139.65 },
+  dubai: { name: "Dubai", lat: 25.2, lon: 55.27 },
+};
