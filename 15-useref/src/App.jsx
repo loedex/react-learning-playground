5da8221ac1952525547e4e4dc1d075e2238837f1
Husnain Ahmad <loedex@yahoo.com>
2026-09-24 16:43:35 +0500

    feat: enhance App component with state management and ref display

--- 15-useref/src/App.jsx
+++ 15-useref/src/App.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import { useRef } from 'react'
+import React from "react";
+import { useRef } from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState(0);
   const myRef = useRef(20);
-  myRef.current = 50;
-  console.log("abc",myRef.current);
-  
-  return (
-    <div>{myRef.current}</div>
-  )
-}
 
-export default App
+  function handleRefChange() {
+    myRef.current++;
+    console.log(`Current Value : ${myRef.current}`);
+  }
+  function handleStateChange() {
+    setValue((prev)=>(prev+1))
+  }
+
+  return (
+    <div>
+      <p>Ref Value : {myRef.current} </p>
+      <p>State Value : {value}</p>
+      <button onClick={handleRefChange}>Change myRef.current</button>
+      <button onClick={handleStateChange}>Change state value</button>
+    </div>
+  );
+};
+
+export default App;
